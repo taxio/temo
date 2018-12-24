6fd7a1bf017c38e82432669bc8f33842cc5edb65
taxio <taxio.2029@gmail.com>
2018-12-24 20:43:43 +0900

    package

---
+++ build-macos.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+APP="Example.app"
+mkdir -p $APP/Contents/{MacOS,Resources}
+go build -ldflags "-X main.staticDir=$(pwd)/Example.app/Contents/Resources/app" -o $APP/Contents/MacOS/lorca-example 
+cat > $APP/Contents/Info.plist << EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleExecutable</key>
+	<string>lorca-example</string>
+	<key>CFBundleIconFile</key>
+	<string>icon.icns</string>
+	<key>CFBundleIdentifier</key>
+	<string>com.zserge.lorca.example</string>
+</dict>
+</plist>
+EOF
+cp icons/icon.icns $APP/Contents/Resources/icon.icns
+cp -r app/build/ $APP/Contents/Resources/app
+find $APP
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gin-contrib/static"
@@ -8,7 +9,10 @@ import (
 	"github.com/zserge/lorca"
 )
 
+var staticDir = "./app/build"
+
 func main() {
+	fmt.Println(staticDir)
 	go runServer()
 
 	err := runUI()
@@ -17,6 +21,17 @@ func main() {
 	}
 }
 
+// app/build/以下を配信するサーバー
+func runServer() {
+	router := gin.Default()
+	router.Use(static.Serve("/", static.LocalFile(staticDir, true)))
+	err := router.Run(":9000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// lorca起動
 func runUI() error {
 	ui, err := lorca.New("", "", 320, 480)
 	if err != nil {
@@ -26,10 +41,4 @@ func runUI() error {
 	ui.Load("http://localhost:9000")
 	<-ui.Done()
 	return nil
-}
-
-func runServer() {
-	router := gin.Default()
-	router.Use(static.Serve("/", static.LocalFile("./app/build", true)))
-	router.Run(":9000")
 }
